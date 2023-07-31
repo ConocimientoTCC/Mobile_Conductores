afbbd78d94d5d42061f2eaa7c496e6081b172d18
felsh123 <jufegira2000@hotmail.com>
2023-07-31 16:43:25 -0500

    Estado Mecanico

---
+++ Assets/Scripts/EstadoMecanico/Question_Mec.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Question_Mec
+{
+    public string[] answers; // Arreglo de respuestas (2 respuestas en este caso)
+    public int correctAnswerIndex; // Índice de la respuesta correcta
+}
+
---
+++ Assets/Scripts/EstadoMecanico/QuizManager_Mec.cs
@@ -0,0 +1,87 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuizManager_Mec : MonoBehaviour
+{
+    public Question_Mec[] questions; // Lista de preguntas que muestra este panel
+    public GameObject resultPanel; // Panel para mostrar la alerta de resultado
+    public GameObject finishButton; // Botón para terminar el cuestionario
+
+    private bool isQuizFinished = false; // Bandera para indicar si el cuestionario está terminado
+
+    public void StartQuiz()
+    {
+        // Mostrar todas las preguntas en este panel
+        foreach (Question_Mec question in questions)
+        {
+            // Aquí podrías acceder a la pregunta, sus respuestas y configurar los botones en el panel
+            // Para cada pregunta, también deberías asignar un identificador único o usar el índice dentro del array.
+            // Por ejemplo, podrías usar el índice del loop como identificador.
+        }
+    }
+
+    public void SelectAnswer(int questionIdentifier, int selectedAnswerIndex)
+    {
+        if (!isQuizFinished)
+        {
+            // Obtener la pregunta actual por su identificador único o índice en el array de preguntas
+            Question_Mec currentQuestion = questions[questionIdentifier];
+
+            // Verificar si la respuesta es correcta o incorrecta
+            bool isCorrect = selectedAnswerIndex == currentQuestion.correctAnswerIndex;
+
+            // Mostrar la alerta de resultado y manejar la lógica para mostrar la siguiente pregunta o finalizar el cuestionario
+            ShowResult(isCorrect);
+        }
+    }
+
+    private void ShowResult(bool isCorrect)
+    {
+        // Mostrar la alerta (panel) de resultado correcto o incorrecto
+        resultPanel.SetActive(true);
+
+        // Aquí podrías manejar la lógica para pasar a la siguiente pregunta o finalizar el cuestionario.
+        // Puedes implementar un sistema de navegación entre preguntas dentro del mismo panel.
+        // Si es el último panel, puedes activar el botón de "Terminado" en el QuizManager.
+
+        // Ejemplo de cómo podría ser la lógica para pasar a la siguiente pregunta:
+        // Al finalizar el resultado, desactivas la alerta y pasas a la siguiente pregunta en el panel.
+        // Si no hay más preguntas, activas el botón de "Terminado" en el QuizManager.
+
+        // Pasar a la siguiente pregunta después de un breve retraso (2 segundos en este caso)
+        Invoke("ShowNextQuestion", 2f);
+    }
+
+    private void ShowNextQuestion()
+    {
+        // Desactivar la alerta de resultado
+        resultPanel.SetActive(false);
+
+        // Aquí podrías implementar la lógica para mostrar la siguiente pregunta dentro de este panel.
+        // Por ejemplo, puedes desactivar la pregunta actual y activar la siguiente pregunta.
+
+        // Si no hay más preguntas, podrías activar el botón de "Terminado" en el QuizManager
+        // o pasar al siguiente panel si tienes más paneles con preguntas.
+
+        // Ejemplo:
+        // Desactivar pregunta actual
+        // currentQuestion.SetActive(false);
+
+        // Obtener el índice de la siguiente pregunta
+        // int nextQuestionIndex = currentQuestionIndex + 1;
+
+        // Si hay más preguntas en este panel
+        // if (nextQuestionIndex < questions.Length)
+        // {
+        //     // Activar la siguiente pregunta
+        //     questions[nextQuestionIndex].SetActive(true);
+        // }
+        // else
+        // {
+        //     // Si no hay más preguntas, el cuestionario está terminado en este panel
+        //     // Activa el botón de "Terminado" en el QuizManager
+        //     isQuizFinished = true;
+        //     finishButton.SetActive(true);
+        // }
+    }
+}
